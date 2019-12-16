13d866bbcf75eb6b73434a74a546deb57c6c54ca
Alice <alicesunhi@gmail.com>
2019-12-16 13:53:23 -0800

    api calls set up on app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: [],
+      customers: [],
+      error: ""
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/customers")
+      .then(response => {
+        this.setState({
+          customers: response.data
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: "there was an error"
+        });
+      });
+
+    axios
+      .get("http://localhost:3000/movies")
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: "there was an error"
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +49,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro"></p>
       </div>
     );
   }
---
+++ src/components/Movie.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Movie.css";
+import PropTypes from "prop-types";
+
+const Movie = props => {
+  return <div></div>;
+};
