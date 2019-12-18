0395a4ddd96c8db8fbc9a59aadeee05f1707eeab
Alice <alicesunhi@gmail.com>
2019-12-18 09:35:31 -0800

    fixed lorem ipsum text

--- src/App.js
+++ src/App.js
@@ -221,15 +221,14 @@ class App extends Component {
                 <Jumbotron>
                   <h1>Video Store</h1>
                   <p>
-                    // Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed // do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua. // Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco // laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute // irure dolor in reprehenderit
-                    in voluptate velit esse cillum // dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat // cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est
-                    laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
                   </p>
                 </Jumbotron>
               </div>
