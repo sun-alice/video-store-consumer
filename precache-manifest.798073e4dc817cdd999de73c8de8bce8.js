c81db2bf1d022ed1a44a308b5ed969e1a51973eb
Rinostar <codingrinostar@gmail.com>
2019-12-18 21:56:15 -0800

    Updates

---
+++ precache-manifest.798073e4dc817cdd999de73c8de8bce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a169ed65586322ffdbe75d0e1623e1e9",
+    "url": "/video-store-consumer/index.html"
+  },
+  {
+    "revision": "7dced655138cd099f936",
+    "url": "/video-store-consumer/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "990e70eeff63c0060dda",
+    "url": "/video-store-consumer/static/css/main.3476475e.chunk.css"
+  },
+  {
+    "revision": "7dced655138cd099f936",
+    "url": "/video-store-consumer/static/js/2.290fee6e.chunk.js"
+  },
+  {
+    "revision": "990e70eeff63c0060dda",
+    "url": "/video-store-consumer/static/js/main.93fde3b0.chunk.js"
+  },
+  {
+    "revision": "1fd30c708e6bf2ff8eb8",
+    "url": "/video-store-consumer/static/js/runtime~main.4e1f96c7.js"
+  }
+]);
